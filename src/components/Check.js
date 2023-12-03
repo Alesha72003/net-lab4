b5e3d14d440c2fd4c24b6145392eff81f1b70165
Alexey Slokva <Alesha72003@ya.ru>
2023-12-03 17:57:30 +0300

    peee pooo

--- src/components/Check.js
+++ src/components/Check.js
@@ -16,7 +16,7 @@ export default function Check() {
             content: {
                name,
                group,
-               part
+               part: Number(part);
             },
             sign
         }).then((response) => {
